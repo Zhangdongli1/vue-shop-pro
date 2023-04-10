74bf4103cc9502826651f070b380e6a3344a46bc
Zhangdongli1 <1768526825@qq.com>
2023-04-10 19:30:14 +0800

    chore(global): 更新了代码规范
    n

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+console.log('123');
 
 createApp(App).use(store).use(router).mount('#app')
