c26feb97cf059361f45413aa779993cc23a79f05
Zhangdongli1 <1768526825@qq.com>
2023-04-10 19:31:40 +0800

    chore(global): 测试代码是否配置成功

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-console.log('123');
+console.log('123')
+console.log('1234')
 
 createApp(App).use(store).use(router).mount('#app')
